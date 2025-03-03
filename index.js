5bd9b20cdb5eb9c28beef53866455f253c55ba5a
Rachel-M09 <129060558+Rachel-M09@users.noreply.github.com>
2025-03-03 19:47:51 +0200

    Added some basic JS for the search bar and h2

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-//alert ("hello")
+function searchWeather(event) {
+    event.preventDefault();
+    let searchInput = document.querySelector("#search-bar");
+
+    let h2 = document.querySelector("#city");
+    h2.innerHTML = `${searchInput.value}`
+
+}
+
+
+let form = document.querySelector("form");
+form.addEventListener("submit", searchWeather);
