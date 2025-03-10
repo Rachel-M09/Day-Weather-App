769bb0f3a0db157ed50f393b98b6f85721002a13
Rachel-M09 <129060558+Rachel-M09@users.noreply.github.com>
2025-03-10 12:13:58 +0200

    Updated weather app API

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
+function updateWeather(response){
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
+    let temperature = response.data.temperature.current;
+
+    temperatureElement.innerHTML = Math.round(temperature);
+    cityElement.innerHTML = response.data.city
+}
+
+function searchCity(city){
+    apiKey ="abatc7e4c535e39o4038301f01bf3de5";
+    apiUrl =`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(apiUrl).then(updateWeather);
+}
+
 function searchWeather(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-bar");
 
-    let h2 = document.querySelector("#city");
-    h2.innerHTML = `${searchInput.value}`
-
+    searchCity(searchInput.value);
 }
 
 
 let form = document.querySelector("form");
 form.addEventListener("submit", searchWeather);
+
+searchCity("Johanesburg")
