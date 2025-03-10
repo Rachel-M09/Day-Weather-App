1c262f2b3b70493e156a7a143c722f26afa9e042
Rachel-M09 <129060558+Rachel-M09@users.noreply.github.com>
2025-03-10 14:35:05 +0200

    Updated the temperature data and the API

--- index.js
+++ index.js
@@ -2,9 +2,39 @@ function updateWeather(response){
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
     let temperature = response.data.temperature.current;
+    let conditionElement = document.querySelector("#condition");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    let timeElement = document.querySelector("#date");
+    let date = new Date(response.data.time * 1000);
 
     temperatureElement.innerHTML = Math.round(temperature);
-    cityElement.innerHTML = response.data.city
+    cityElement.innerHTML = response.data.city;  
+    conditionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windElement.innerHTML = `${response.data.wind.speed} km/h`;
+    timeElement.innerHTML = today(date);
+}
+
+function today(date){
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city){
