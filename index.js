25fe67cc236c1c048bc89cf68ccc12ca04cc6e5e
Rachel-M09 <129060558+Rachel-M09@users.noreply.github.com>
2025-03-12 15:52:25 +0200

    Updated the icon API, removed the 'Change Theme' button, and added the weather forecast

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function updateWeather(response){
     let windElement = document.querySelector("#wind");
     let timeElement = document.querySelector("#date");
     let date = new Date(response.data.time * 1000);
+    let iconElement = document.querySelector("#icon");
 
     temperatureElement.innerHTML = Math.round(temperature);
     cityElement.innerHTML = response.data.city;  
@@ -14,6 +15,8 @@ function updateWeather(response){
     humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
     windElement.innerHTML = `${response.data.wind.speed} km/h`;
     timeElement.innerHTML = today(date);
+    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}"/>`
+   
 }
 
 function today(date){
